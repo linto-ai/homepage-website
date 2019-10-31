3ce44459523b1d89f3d4628717b61e9cf11de685
rlopez <rlopez@linagora.com>
2019-10-31 14:30:14 +0100

    update links + test contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  
+
   $('.submit-btn').on('click', function () {
     handleForm()
   })
-  
- 
+
+
   function testName () {
     const regex = /^[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ\s\-\']+$/
     const nameField = $('#contact-name')
@@ -17,7 +17,7 @@ $(document).ready(function(){
       nameError.html('Champs requis')
       nameField.addClass('error')
       return false
-    } else if (!regex.test(name)) { 
+    } else if (!regex.test(name)) {
       nameError.html('Veuillez saisir un nom valide')
       nameField.addClass('error')
       return false
@@ -25,7 +25,7 @@ $(document).ready(function(){
       return true
     }
   }
-  
+
   function testEmail () {
     const regex = /^[a-z]{1}[a-z0-9\-\.\_]*[a-z0-9]+[\@]+[a-z]{1}[a-z0-9\-\.]*[a-z0-9]+[\.]+[a-z]{2,4}$/
     const emailField = $('#contact-email')
@@ -38,7 +38,7 @@ $(document).ready(function(){
       emailError.html('Champs requis')
       emailField.addClass('error')
       return false
-    } else if (!regex.test(email)) { 
+    } else if (!regex.test(email)) {
       emailError.html('Veuillez saisir une adresse email valide')
       emailField.addClass('error')
       return false
@@ -46,7 +46,7 @@ $(document).ready(function(){
       return true
     }
   }
-  
+
   function testPhone () {
     const regex = /^\d{8,20}$/
     const phoneField = $('#contact-phone')
@@ -59,17 +59,19 @@ $(document).ready(function(){
     if (phone.length === 0) {
       return true
     } else {
-      if (!regex.test(phone)) { 
+      if (!regex.test(phone)) {
         phoneError.html('Veuillez saisir un numéro de téléphone valide')
         phoneField.addClass('error')
         return false
+      } else {
+        return true
       }
     }
   }
 
   function testSociety () {
     regex = /^[a-zA-ZÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ\s\-\']+$/
-    
+
     const societyField = $('#contact-society')
     const societyError = $('.error-field.society')
     const society = societyField.val()
@@ -80,14 +82,16 @@ $(document).ready(function(){
     if (society.length === 0) {
       return true
     } else {
-      if (!regex.test(society)) { 
+      if (!regex.test(society)) {
         societyError.html('Veuillez saisir un nom de société valide')
         societyField.addClass('error')
         return false
+      } else {
+        return true
       }
     }
   }
-  
+
   function testMessage () {
     const msgField = $('#contact-msg')
     const msgError = $('.error-field.msg')
@@ -100,7 +104,7 @@ $(document).ready(function(){
       msgError.html('Champs requis')
       msgField.addClass('error')
       return false
-    } else if (msg.includes('<') || msg.includes('>') || msg.includes('script') ||  msg.includes('|')) { 
+    } else if (msg.includes('<') || msg.includes('>') || msg.includes('script') ||  msg.includes('|')) {
       msgError.html('Le message contient des caractères non autorisés')
       msgField.addClass('error')
       return false
@@ -118,11 +122,12 @@ $(document).ready(function(){
     let phoneValid = testPhone()
     let societyValid = testSociety()
     let msgValid = testMessage()
-    
-    if(!nameValid || !emailValid || !phoneValid || !societyValid || msgValid) {
+
+    if(!nameValid || !emailValid || !phoneValid || !societyValid || !msgValid) {
       isValid = false
       return false
-    } else {
+    }
+    if (isValid){
       sendForm()
     }
   }
@@ -137,8 +142,23 @@ $(document).ready(function(){
       society: $('#contact-society').val(),
       message: $('#contact-msg').val()
     }
-    console.log('Form valid')
-    console.log(payload)
+    console.log('send payload:', payload)
+    fetch('https://gamma.linto.ai/mail/send', {
+        method: 'post',
+        body:JSON.stringify(payload)
+    }).then((res) => res.json())
+    .then(function (data) {
+      console.log(data)
+      if (data === 'mailSend') {
+        console.log('mail: success')
+        $('#contact-msg').innerHTML = ''
+      } else {
+        throw 'Une erreur est survenue.'
+      }
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
   }
 
   document.getElementById('contact-name').onblur = function () {
