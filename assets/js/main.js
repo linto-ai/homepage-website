2f5fbaa8a7062323cf70a0b2977600e9a318586f
Damien LAINÉ <damien.laine@gmail.com>
2019-11-15 16:53:02 +0100

    add some splash modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
   const cookieLang = getCookie('userLang')
   let currentPageLang = 'en'
-  let basePath = '' 
-  if(window.location.host === 'linto-ai.github.io') {
-    basePath ='https://linto-ai.github.io/homepage-website'
-  } else if (window.location === 'linto-ai.local'){
-    basePath ='http://linto-ai.local'
+  let basePath = ''
+  if (window.location.host === 'linto-ai.github.io') {
+    basePath = 'https://linto-ai.github.io/homepage-website'
+  } else if (window.location === 'linto-ai.local') {
+    basePath = 'http://linto-ai.local'
   }
 
   if (window.location.href.indexOf('/fr') >= 0) {
     currentPageLang = 'fr'
   }
+
+  //modal
+  $("#ex1").modal({
+    fadeDuration: 100
+  });
 
   // Toggle page language menu
   $('#lang-btn').on('click', function () {
@@ -24,7 +29,7 @@ $(document).ready(function () {
   })
 
   // Page language update
-  $('.lang-update').on('click', function() {
+  $('.lang-update').on('click', function () {
     const target = $(this).attr('data-href')
     const lang = $(this).attr('data-lang')
     setCookie('userLang', lang, 31)
@@ -35,14 +40,14 @@ $(document).ready(function () {
 
   // BURGER NAV
   $('#burger-menu').on('click', function () {
-    if($(this).hasClass('closed')){
+    if ($(this).hasClass('closed')) {
       $(this).removeClass('closed').addClass('opened')
       $('#burger-menu-nav').removeClass('hidden')
-      $('header').css('height','100%')
+      $('header').css('height', '100%')
     } else {
       $(this).removeClass('opened').addClass('closed')
       $('#burger-menu-nav').addClass('hidden')
-      $('header').css('height','40px')
+      $('header').css('height', '40px')
     }
   })
 
@@ -50,18 +55,18 @@ $(document).ready(function () {
   $('.nav-link').on('click', function () {
     if ($(this).attr('href').indexOf('#') >= 0) {
       const burgerMenuDisplay = $('#burger-nav').css('display')
-      if(burgerMenuDisplay === 'flex') {
+      if (burgerMenuDisplay === 'flex') {
         $('#burger-menu').removeClass('opened').addClass('closed')
         $('#burger-menu-nav').addClass('hidden')
-        $('header').css('height','40px')
+        $('header').css('height', '40px')
       }
     }
   })
 
-  function setCookie (cname, cvalue, exdays) {
+  function setCookie(cname, cvalue, exdays) {
     var d = new Date()
-    d.setTime(d.getTime() + (exdays*24*60*60*1000))
-    var expires = "expires="+ d.toUTCString()
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+    var expires = "expires=" + d.toUTCString()
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
   }
 
@@ -69,7 +74,7 @@ $(document).ready(function () {
     var name = cname + "="
     var decodedCookie = decodeURIComponent(document.cookie)
     var ca = decodedCookie.split(';')
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
       var c = ca[i]
       while (c.charAt(0) == ' ') {
         c = c.substring(1)
