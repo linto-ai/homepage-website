092ce32e2b1cacb8ee2881bc409438830c5d8a34
rlopezdev <rlopez@linagora.com>
2020-09-24 14:23:53 +0200

    integrate Setup page structure

--- runtime/components/WebServer/routes/routes.js
+++ runtime/components/WebServer/routes/routes.js
@@ -3,7 +3,7 @@ const debug = require('debug')('app:webserver:routes')
 module.exports = (webServer) => {
     return {
         "/": require('./homepage')(webServer),
-        "/enterprises": require('./enterprises')(webServer),
+        "/setup": require('./setup')(webServer),
         "/fr": require('./fr')(webServer),
         "/mail": require('./contact')(webServer)
     }
