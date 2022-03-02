e43650a615fb538c146c6b049ba190a3e9c95b01
Damien Laine <damien.laine@gmail.com>
2022-03-02 16:09:01 +0100

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -141,7 +141,7 @@ $(document).ready(function(){
       society: $('#contact-society').val(),
       message: $('#contact-msg').val()
     }
-    axios.post('https://gamma.linto.ai/mail/send', {
+    axios.post('https://dl.linto.ai/mail/send', {
       method: 'post',
       data: payload
     })
